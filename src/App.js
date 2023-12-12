7e797f3d0af3f2f04184b0cf096ffea6d1476749
joshi <joseph.ilagan.99@gmail.com>
2023-12-12 10:21:36 +0800

    styling

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
       <Job salary="12,000" position="Senior SDE" company="Amazon"/>
       <Job salary="9,000" position="Junior SDE" company="Google"/>
-      <Job salary="18,000" position="Project Manager" company="Netflix"/>
-      
+      <Job salary="18,000" position="Project Manager" company="Netflix"/>    
 
     </div>
   );
--- src/Job.js
+++ src/Job.js
@@ -3,7 +3,7 @@ import React from 'react'
 //pass in the props as parameter to the Job Component
 const Job = (props) => {
   return (
-    <div>
+    <div className='jab'>
         <div className='box'>
             {/* use chaining method to declare the props values */}
             <h1 className='position'>{props.position}</h1>
