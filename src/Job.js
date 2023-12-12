d4038a2c0fa5bcfb863243f56d31e70ac19005a8
joshi <joseph.ilagan.99@gmail.com>
2023-12-12 10:10:58 +0800

    notes for props

--- src/Job.js
+++ src/Job.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
-
+//pass in the props as parameter to the Job Component
 const Job = (props) => {
   return (
     <div>
         <div className='box'>
+            {/* use chaining method to declare the props values */}
             <h1 className='position'>{props.position}</h1>
             <h3 className='description'> Salary: {props.salary} - Company: {props.company}</h3>
         </div>        
